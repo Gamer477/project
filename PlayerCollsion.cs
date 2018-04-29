85c36ffa18f7b4028f0dcb2f9d4da7b53452ad49
Gamer477 <38445766+Gamer477@users.noreply.github.com>
2018-04-29 23:43:59 +0200

    Graphics
    jumangi

---
+++ PlayerCollsion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollsion : MonoBehaviour {
+    public PlayerMovement movment;
+    void OnCollisionEnter(Collision collisiioninfo)
+    {
+        if (collisiioninfo.collider.tag == "cube")
+        {
+            movment.enabled = false;
+            Debug.Log("sssdssda");
+          
+        }
+    }
+}
---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+    public void PlayeGame()
+    {
+        SceneManager.LoadScene("seen2");
+        Time.timeScale=1f;
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
